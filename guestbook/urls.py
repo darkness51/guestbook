0d68afadc5411ebcbf40e8ab04392257a8c5f8ff
Jonatan <jrperdomo.24@gmail.com>
2012-11-27 18:39:29 +0000

    edito la vista 'editar' y agrego la funcion eliminar

--- guestbook/urls.py
+++ guestbook/urls.py
@@ -4,13 +4,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'main.views.index', name='home'),
-    url(r'^guestbook/add', 'main.views.agregar', name="agregar"),
-    url(r'^guestbook/(?P<id>\d+)/edit', 'main.views.editar', name="editar"),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'^$', 'main.views.index', name='home'),
+	url(r'^guestbook/add', 'main.views.agregar', name='agregar'),
+	url(r'^guestbook/edit/(?P<id>\d+)', 'main.views.editar', name='editar'),
+	url(r'^guestbook/del/(?P<id>\d+)', 'main.views.eliminar', name='eliminar'),
 )

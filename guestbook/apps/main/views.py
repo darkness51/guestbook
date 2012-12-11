5539ac9a6b310c7b8935d2b0228de30a53525fd2
Carlos Aguilar <caguilar@dwdandsolutions.com>
2012-12-11 11:45:40 -0600

    Modificado el codigo para cumplir la PEP8 y algunas mejoras de estilo en las plantillas

--- guestbook/apps/main/views.py
+++ guestbook/apps/main/views.py
@@ -1,42 +1,49 @@
-from django.conf		import settings
-from django.core.mail		import send_mail
-from django.http		import HttpResponseRedirect,HttpResponse,HttpResponseForbidden, Http404
-from django.shortcuts 		import render_to_response, get_object_or_404, get_list_or_404
-from django.template	  	import RequestContext
-from django.views.generic	import TemplateView, FormView
-from main.forms			import MessageForm
-from main.models		import Message
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from main.forms import MessageForm
+from main.models import Message
 
 def index(request):
-	visits = Message.objects.all()
-	data = {'visits': visits}
-	return render_to_response("index.html",data,context_instance=RequestContext(request))
+    visits = Message.objects.all()
+    data = {
+        'visits': visits
+    }
+
+    return render_to_response("index.html", data, context_instance=RequestContext(request))
 
 def agregar(request):
-	if request.method == "POST":
-		form = MessageForm(request.POST)
-		if form.is_valid():
-			form.save()
-			return HttpResponseRedirect('/')
-	else:
-		form = MessageForm()
-	data = {'form' : form,}
-	return render_to_response("add.html",data,context_instance=RequestContext(request))
+    if request.method == "POST":
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    else:
+        form = MessageForm()
 
-def editar(request,id):
-	message = Message.objects.get(pk=id)
-	if request.method == "POST":
-		form = MessageForm(request.POST, instance=message)
-		if form.is_valid():
-			form.save()
-			return HttpResponseRedirect('/')
-	else:
-		form = MessageForm(instance=message)
-	data = {'form' : form}
-	return render_to_response("edit.html", data, context_instance=RequestContext(request))
+    data = {
+        'form': form,
+    }
 
-def eliminar(request,id):
-	#Podriamos solicitar confirmacion antes de proceder...
-	message = Message.objects.get(pk=id) #instancia
-	message.delete()
-	return HttpResponseRedirect('/')
+    return render_to_response("add.html", data, context_instance=RequestContext(request))
+
+def editar(request, id):
+    message = get_object_or_404(Message, pk=id)
+    if request.method == "POST":
+        form = MessageForm(request.POST, instance=message)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    else:
+        form = MessageForm(instance=message)
+
+    data = {
+        'form': form
+    }
+    return render_to_response("edit.html", data, context_instance=RequestContext(request))
+
+def eliminar(request, id):
+    #Podriamos solicitar confirmacion antes de proceder...
+    message = get_object_or_404(Message, pk=id) #instancia
+    message.delete()
+    return HttpResponseRedirect('/')
